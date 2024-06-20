0e682a36e51f636e70fc4cd437cb13ce363fd71e
sang.ngo <sang.ngo@inceptionlabs.info>
2024-06-20 14:53:10 +0700

    chore: fix some warning

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,11 @@
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ProfileService } from '../../services/profile.service';
-import { AlertController, LoadingController } from '@ionic/angular';
-import { firstValueFrom } from 'rxjs';
+import {IonicModule, LoadingController} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {Component} from '@angular/core';
+import {ProfileService} from '../../services/profile.service';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -28,20 +27,18 @@ export class ProfileComponent {
 
   constructor(
     private profileService: ProfileService,
-    private alertController: AlertController,
     private loadingController: LoadingController
-  ) {}
+  ) {
+  }
 
   async ionViewWillEnter() {
-    this.fetchProfile();
-    console.log(1232);
+    await this.fetchProfile();
   }
 
   async fetchProfile() {
-    this.presentLoading();
+    await this.presentLoading();
     try {
-      const response = await firstValueFrom(this.profileService.getProfile());
-      this.profile = response;
+      this.profile = await firstValueFrom(this.profileService.getProfile());
     } catch (error) {
       this.errorMessage = 'Error fetching profile.';
       console.error('Profile fetch error:', error);
@@ -58,14 +55,5 @@ export class ProfileComponent {
       duration: 5000, // Optional timeout
     });
     await this.loading.present();
-  }
-
-  async showErrorAlert(header: string, message: string) {
-    const alert = await this.alertController.create({
-      header,
-      message,
-      buttons: ['OK'],
-    });
-    await alert.present();
   }
 }
